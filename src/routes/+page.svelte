5f3208caf60e4aad26a8f105719a2c259eec6cbd
Max Petretta <git@maxpetretta.com>
2023-10-15 21:05:17 -0400

    feat: remove checkbox for actions app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import clsx from "clsx"
-  
   const shortcutUrl = import.meta.env.VITE_SHORTCUT_URL
-  let hasAgreed = false
 </script>
 
 <div class="max-w-xl flex flex-col items-center">
@@ -13,18 +10,11 @@
       href="https://neynar.xyz">Neynar</a
     >.
   </p>
-  
-  <p class="mt-4 text-center">Quickcast relies on the free <a class="underline underline-offset-2" href="https://apps.apple.com/us/app/actions/id1586435171">Actions</a> app, which <strong>must</strong> be installed before downloading the shortcut.</p>
-  
+
   <a
-    class={clsx("mt-10 px-10 py-3 rounded-full text-black font-bold shadow-md transition-all duration-200",
-    {"bg-gray-400 pointer-events-none": !hasAgreed},
-    {"bg-white pointer-events-auto hover:shadow-lg hover:scale-105": hasAgreed})}
+    class="mt-8 px-10 py-3 rounded-full text-black font-bold shadow-md transition-all duration-200 bg-white pointer-events-auto hover:shadow-lg hover:scale-105"
     href={shortcutUrl}>
     Download Shortcut
   </a>
-  <div class="flex items-center mt-4">
-    <input type="checkbox" bind:checked={hasAgreed} />
-    <span class="ml-1.5">I have installed the <a class="underline underline-offset-2" href="https://apps.apple.com/us/app/actions/id1586435171">Actions app</a></span>
-  </div>
+
 </div>
